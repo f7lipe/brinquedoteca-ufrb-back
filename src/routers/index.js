4dc32b7bd3bf9291d2c4df7e0d3bc4d47a85bce2
= <f7lipe@me.com>
2022-07-11 10:16:52 -0300

    feat: create card

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import signInRouter from "./signinRouter.js";
 
 const router = Router()
+router.use(signInRouter)
 
 export {router}
+
+//autenticação (login, recuperar senha, criar conta)
+//controle de pemanência 
+//cadastro de crianças e responsáveis
+
