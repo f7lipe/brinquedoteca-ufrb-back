fd7ac7c9613a8561a11fdb633d3e9787fb2d3265
Deivid Gomes <davidsilva29@hotmail.com>
2022-07-24 21:07:26 -0300

    fixing detail in database sql
    Changing "chidrens" to "childrens"

--- src/config/database.sql
+++ src/config/database.sql
@@ -13,11 +13,11 @@ CREATE TABLE "childrens" (
 
 
 
-CREATE TABLE "chidrens_guardians" (
+CREATE TABLE "childrens_guardians" (
 	"id" serial NOT NULL,
 	"children_id" int NOT NULL,
 	"guardian_id" int NOT NULL,
-	CONSTRAINT "chidrens_guardians_pk" PRIMARY KEY ("id")
+	CONSTRAINT "childrens_guardians_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -56,8 +56,8 @@ CREATE TABLE "guardians" (
 
 
 
-ALTER TABLE "chidrens_guardians" ADD CONSTRAINT "chidrens_guardians_fk0" FOREIGN KEY ("children_id") REFERENCES "childrens"("id");
-ALTER TABLE "chidrens_guardians" ADD CONSTRAINT "chidrens_guardians_fk1" FOREIGN KEY ("guardian_id") REFERENCES "guardians"("id");
+ALTER TABLE "childrens_guardians" ADD CONSTRAINT "childrens_guardians_fk0" FOREIGN KEY ("children_id") REFERENCES "childrens"("id");
+ALTER TABLE "childrens_guardians" ADD CONSTRAINT "childrens_guardians_fk1" FOREIGN KEY ("guardian_id") REFERENCES "guardians"("id");
 
 ALTER TABLE "permanence" ADD CONSTRAINT "permanence_fk0" FOREIGN KEY ("children_id") REFERENCES "childrens"("id");
 ALTER TABLE "permanence" ADD CONSTRAINT "permanence_fk1" FOREIGN KEY ("guardian_entrance_id") REFERENCES "guardians"("id");
