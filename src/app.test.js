5f2a2bf08e159a37edbd3253b9b058d28985cff4
Deivid Gomes <davidsilva29@hotmail.com>
2022-08-05 11:25:22 -0300

    Implementando teste unitário

--- src/app.test.js
+++ src/app.test.js
@@ -1,15 +1,36 @@
 import * as childrenService from './services/childrenServices.js';
+import * as childrenRepository from './repositories/childrenRepository.js';
+
+const children = {
+    cpf: "867577",
+    name: "John",
+    birthDate: "august-01-2000",
+    address: "Rua dois, nº 0"
+}
 
 describe("Children Tests Suit", () => {
-    it("should create a children given valid credentials", async()=>{
-        const children = {
-            cpf: "866666",
-            name: "John Doe",
-            birthDate: "august-01-2000",
-            address: "Rua dois, nº 0"
-    }
+    it("should create a children given valid credentials", async () => {       
         const response = await childrenService.createChildren(children);
         console.log(response);
         expect(response.id).toBeDefined();
     })
 })
+
+describe("Children test unit", () => {
+    beforeEach(async () => {
+        const {id} = await childrenRepository.findChildren(children.cpf);
+        console.log(id);
+        if (id){
+            await childrenRepository.deleteChildren(id);
+        }
+    });
+    it("Test unit", async () => {
+        //const children = recommendationFactory.createRecommendationData();
+        const spy = jest
+            .spyOn(childrenRepository, "createChildren")
+            .mockImplementationOnce(() => { })
+        await childrenService.createChildren(children);
+        expect(spy).toBeCalledWith(children)
+    })
+})/*
+*/
